d1e5e7e45b76eca42baa2dc58ca24994cccedc47
Prafful <praffuljha1234@gmail.com>
2024-09-01 18:49:28 +0530

    Initial commit

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,19 @@
+import Learn from './Learn'
+function App() {
+  const username = "Worker around the code"
+  return (
+
+    <>
+      <h1>
+        Clean  code executed {username} // evaluated expression
+      </h1>
+      <Learn/>
+      <h2> 
+        what the hell is this
+      </h2>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 01basic/src/Learn.jsx
@@ -0,0 +1,12 @@
+
+function Learn(){
+    return (
+        <h1> 
+        Learn and earn using freelancing
+        
+    </h1>
+    )
+  
+}
+
+export default Learn
