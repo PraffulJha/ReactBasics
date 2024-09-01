ab0adef2beb67cfd68b5a578753b608cfc9ce5ef
Prafful <praffuljha1234@gmail.com>
2024-09-01 19:22:31 +0530

    hooks completed

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
 import Learn from './Learn'
 function App() {
-  const username = "Worker around the code"
+  //let counter = 0
+  //useState = responsible for changing of ui state 
+  let [counter,setCounter] = useState(0)
+  const addValue = () => {
+      //counter++;
+      setCounter(++counter)
+      console.log("Clicked",counter);
+  }
+  const removeValue = () => {
+    //counter++;
+    if(counter > 0){
+      --counter;
+    }else {
+      counter = 0;
+    }
+    setCounter(counter)
+    console.log("Clicked",counter);
+}
   return (
-
+   
     <>
       <h1>
-        Clean  code executed {username} // evaluated expression
+       Welcome to future
       </h1>
-      <Learn/>
-      <h2> 
-        what the hell is this
-      </h2>
+      <h2>Count : {counter}</h2>
+      <button onClick={addValue} > Add button</button>
+      <button onClick={removeValue}> Remove value</button>
       
     </>
   )
